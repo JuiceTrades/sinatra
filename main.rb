03dc6a06d1364219fed4d4fbb3f695671b1c2770
Domitrius Clark <domitriusclark@Domitriuss-MacBook-Air.local>
2017-02-08 16:46:40 -0500

    Linked sendgrid with contact form

--- main.rb
+++ main.rb
@@ -1,4 +1,33 @@
 require "sinatra"
+require 'sendgrid-ruby'
+include SendGrid
+
+
+
+
+
+
+
+
+
+
+
+
+# name
+# city
+# phone-number
+# company-name
+# company-add
+# postcode
+# message
+
+
+
+
+
+
+
+
 
 get "/" do
 	
@@ -9,6 +38,29 @@ get "/contact" do
 end
 
 
-get '/contact-form' do
-  erb :contact_form
+get '/contact_form' do
+		
+	erb :contact_form
 end
+
+
+post '/contact_form' do
+
+	email_add = params["email"]
+
+	from = Email.new(email: email_add)
+	to = Email.new(email: 'domitrius.anthony@hotmail.com')
+	subject = 'Sending with SendGrid is Fun'
+	content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+	mail = Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	puts response.status_code
+	puts response.body
+	puts response.headers
+
+
+end
+
+
