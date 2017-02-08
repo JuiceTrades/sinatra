071455b5e85288650c4cff326e21f223554f2751
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-08 11:14:31 -0500

    portfolio page version 1.0

--- main.rb
+++ main.rb
@@ -1,10 +1,15 @@
 require "sinatra"
 
 get "/" do
-	
+	# erb :index
+	"hello world"
 end
 
-get "/about" do
+# get "/about" do
+# 	erb :about
+# end
 
+get '/porfolio' do     
+	erb :porfolio
 end
 
