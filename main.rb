1af4fac64ebe509453af45001ff9a4245ac177bd
Chuong Nguyen <tinchuong@yahoo.fr>
2017-02-08 15:42:01 -0500

    all

--- main.rb
+++ main.rb
@@ -1,15 +1,70 @@
 require "sinatra"
 
 get "/" do
-	# erb :index
-	"hello world"
+	erb :index
 end
 
-# get "/about" do
-# 	erb :about
-# end
+get "/home" do
+	erb :home
+end
+
+get "/about" do
+	erb :about
+end
+
+get "/contact" do
+	erb :contact
+end
 
 get '/porfolio' do     
 	erb :porfolio
 end
 
+# we just do 6 pages for the first 6 projects. 
+get '/project1' do     
+	erb :project_1
+end
+
+get '/project2' do     
+	erb :project_2
+end
+
+get '/project3' do     
+	erb :project_3
+end
+
+get '/project4' do     
+	erb :project_4
+end
+
+get '/project5' do     
+	erb :project_5
+end
+
+get '/project6' do     
+	erb :project_6
+end
+
+get '/project7' do     
+	"project7"
+end
+
+get '/project8' do     
+	"project8"
+end
+
+get '/project9' do     
+	"project9"
+end
+
+get '/project10' do     
+	"project10"
+end
+
+get '/project11' do     
+	"project11"
+end
+
+get '/project12' do     
+	"project12"
+end
