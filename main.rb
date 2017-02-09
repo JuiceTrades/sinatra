e2fc088663c69509ff58451889dfc3129f9e6880
Domitrius Clark <domitriusclark@Domitriuss-MacBook-Air.local>
2017-02-09 11:39:07 -0500

    Added customer form

--- main.rb
+++ main.rb
@@ -43,15 +43,29 @@ get '/contact_form' do
 	erb :contact_form
 end
 
+get '/customer_form' do
+	erb :customer_form
+end
+
 
 post '/contact_form' do
 
 	email_add = params["email"]
+	
+	body = params["message"]
+	phone_number = params['phone']
+	city = params['ville']
+	area_code = params['postal']
+	company_add = params['adresse']
+	company_name = params['society']
+	personal_name = params['prenom']
+	body_content = [personal_name, company_name, company_add, city, area_code, phone_number, body]
+
 
 	from = Email.new(email: email_add)
 	to = Email.new(email: 'domitrius.anthony@hotmail.com')
-	subject = 'Sending with SendGrid is Fun'
-	content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+	subject = Subject.new(email_subject)
+	content = Content.new(type: 'text/plain', value: body_content)
 	mail = Mail.new(from, subject, to, content)
 
 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
